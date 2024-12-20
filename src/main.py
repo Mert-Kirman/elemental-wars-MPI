cccd39b3c2519c139e1e6ac3611c934a3df881f0
Mert Kirman <mertkirman@Merts-MacBook-Air.local>
2024-12-21 01:15:53 +0300

    Implement action_phase function

--- src/main.py
+++ src/main.py
@@ -14,6 +14,167 @@ def print_grid(grid):
     print()
 
 
+def movement_phase():
+    '''
+    Function that moves air units to an adjacent empty cell based on their
+    '''
+
+
+def action_phase(grid, unit_partition, top_boundary, bottom_boundary, upper_row_bound, lower_row_bound, N):
+    '''
+    Function that decide whether a unit will attack or skip based on their health and strategic considerations
+    '''
+    action_list = list()
+    for unit in unit_partition:
+        # Units will skip attacking if their health falls below 50%
+        if unit.current_health < unit.max_health // 2:
+            action_list.append(('heal', unit))
+            continue
+
+        enemy_found = False
+
+        # Attack or skip
+        if unit.unit_type == 'earth':
+            attack_directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Directions an earth unit can attack
+            for attack_direction in attack_directions:
+                target_row, target_col = unit.row + attack_direction[0], unit.col + attack_direction[1]
+                # Check if out of original grid borders
+                if target_row < 0 or target_row >= N or target_col < 0 or target_col >= N:
+                    continue
+                
+                # Check if out of partition borders
+                target = None
+                if target_row < upper_row_bound:
+                    index = upper_row_bound - target_row
+                    target = top_boundary[-index][target_col]
+                
+                elif target_row > lower_row_bound:
+                    index = target_row - lower_row_bound - 1
+                    target = bottom_boundary[index][target_col]
+
+                else:   # Inside partition boundaries
+                    index = target_row - upper_row_bound
+                    target = grid[index][target_col]
+
+                if target != '.' and target != 'E': # Enemy found, attack
+                    enemy_found = True
+                    action_list.append(('attack', unit, (target_row, target_col)))
+
+                if not enemy_found: # Skip (heal the unit)
+                    action_list.append(('heal', unit))
+
+        elif unit.unit_type == 'fire':
+            attack_directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]  # Directions a fire unit can attack
+            for attack_direction in attack_directions:
+                target_row, target_col = unit.row + attack_direction[0], unit.col + attack_direction[1]
+                # Check if out of original grid borders
+                if target_row < 0 or target_row >= N or target_col < 0 or target_col >= N:
+                    continue
+                
+                # Check if out of partition borders
+                target = None
+                if target_row < upper_row_bound:
+                    index = upper_row_bound - target_row
+                    target = top_boundary[-index][target_col]
+                
+                elif target_row > lower_row_bound:
+                    index = target_row - lower_row_bound - 1
+                    target = bottom_boundary[index][target_col]
+
+                else:   # Inside partition boundaries
+                    index = target_row - upper_row_bound
+                    target = grid[index][target_col]
+
+                if target != '.' and target != 'F': # Enemy found, attack
+                    enemy_found = True
+                    action_list.append(('attack', unit, (target_row, target_col)))
+
+                if not enemy_found: # Skip (heal the unit)
+                    action_list.append(('heal', unit))
+
+        elif unit.unit_type == 'water':
+            attack_directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]  # Directions a water unit can attack
+            for attack_direction in attack_directions:
+                target_row, target_col = unit.row + attack_direction[0], unit.col + attack_direction[1]
+                # Check if out of original grid borders
+                if target_row < 0 or target_row >= N or target_col < 0 or target_col >= N:
+                    continue
+                
+                # Check if out of partition borders
+                target = None
+                if target_row < upper_row_bound:
+                    index = upper_row_bound - target_row
+                    target = top_boundary[-index][target_col]
+                
+                elif target_row > lower_row_bound:
+                    index = target_row - lower_row_bound - 1
+                    target = bottom_boundary[index][target_col]
+
+                else:   # Inside partition boundaries
+                    index = target_row - upper_row_bound
+                    target = grid[index][target_col]
+
+                if target != '.' and target != 'W': # Enemy found, attack
+                    enemy_found = True
+                    action_list.append(('attack', unit, (target_row, target_col)))
+
+                if not enemy_found: # Skip (heal the unit)
+                    action_list.append(('heal', unit))
+
+        elif unit.unit_type == 'air':
+            attack_directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1), 
+                                 (-2, 0), (2, 0), (0, -2), (0, 2), (-2, -2), (-2, 2), (2, -2), (2, 2)]  # Directions an air unit can attack
+
+            for attack_direction in attack_directions:
+                target_row, target_col = unit.row + attack_direction[0], unit.col + attack_direction[1]
+                # Check if out of original grid borders
+                if target_row < 0 or target_row >= N or target_col < 0 or target_col >= N:
+                    continue
+                
+                # Check if air unit's long range attack is blocked by another unit
+                if attack_direction[0] in [-2, 2] or attack_direction[1] in [-2, 2]:
+                    block_target = None
+                    is_blocked_target_row = attack_direction[0] // 2
+                    is_blocked_target_col = attack_direction[1] // 2
+                    if is_blocked_target_row < upper_row_bound:
+                        index = upper_row_bound - is_blocked_target_row
+                        block_target = top_boundary[-index][is_blocked_target_col]
+                    
+                    elif is_blocked_target_row > lower_row_bound:
+                        index = is_blocked_target_row - lower_row_bound - 1
+                        block_target = bottom_boundary[index][is_blocked_target_col]
+
+                    else:   # Inside partition boundaries
+                        index = is_blocked_target_row - upper_row_bound
+                        block_target = grid[index][is_blocked_target_col]
+
+                    if block_target != '.': # Air unit's long range attack is blocked by another unit
+                        continue
+                    
+                # Check if out of partition borders
+                target = None
+                if target_row < upper_row_bound:
+                    index = upper_row_bound - target_row
+                    target = top_boundary[-index][target_col]
+                
+                elif target_row > lower_row_bound:
+                    index = target_row - lower_row_bound - 1
+                    target = bottom_boundary[index][target_col]
+
+                else:   # Inside partition boundaries
+                    index = target_row - upper_row_bound
+                    target = grid[index][target_col]
+
+                if target != '.' and target != 'A': # Enemy found, attack
+                    enemy_found = True
+                    action_list.append(('attack', unit, (target_row, target_col)))
+
+                if not enemy_found: # Skip (heal the unit)
+                    action_list.append(('heal', unit))
+
+    return action_list
+
+
 if __name__ == "__main__":
     comm = MPI.COMM_WORLD
 
@@ -29,6 +190,7 @@ if __name__ == "__main__":
     ROW_BOUNDS_TAG = 2
     EXTRA_UPPER_3_ROW_TAG = 3   # From the perspective of the receiving worker process
     EXTRA_LOWER_3_ROW_TAG = 4
+    GRID_SIZE_TAG = 5
 
     if rank == 0:
         # Read information from input file
@@ -101,6 +263,7 @@ if __name__ == "__main__":
                     comm.send(grid[upper_row_bound:lower_row_bound + 1], dest=k, tag=GRID_TAG)
                     comm.send(units_partition, dest=k, tag=UNIT_TAG)
                     comm.send(row_bounds, dest=k, tag=ROW_BOUNDS_TAG)
+                    comm.send(N, dest=k, tag=GRID_SIZE_TAG)
 
                 # Receive results from the workers and generate the final grid state
                 # TODO
@@ -115,6 +278,7 @@ if __name__ == "__main__":
         grid = comm.recv(source=0, tag=GRID_TAG)
         unit_partition = comm.recv(source=0, tag=UNIT_TAG)
         row_bounds = comm.recv(source=0, tag=ROW_BOUNDS_TAG)
+        N = comm.recv(source=0, tag=GRID_SIZE_TAG)
 
         upper_row_bound = row_bounds[0]
         lower_row_bound = row_bounds[1]
@@ -138,6 +302,8 @@ if __name__ == "__main__":
                 comm.send(grid[-3:], dest=rank+1, tag=EXTRA_UPPER_3_ROW_TAG)
                 bottom_boundary = comm.recv(source=rank+1, tag=EXTRA_LOWER_3_ROW_TAG)
 
-        print(f'Rank={rank}\n')
-        print_grid(bottom_boundary)
+        # print(f'Rank={rank}\n')
+        # print_grid(bottom_boundary)
+
+        action_list = action_phase(grid, unit_partition, top_boundary, bottom_boundary, upper_row_bound, lower_row_bound, N)
 
--- src/unit.py
+++ src/unit.py
@@ -3,27 +3,32 @@ class Unit:
         self.unit_type = unit_type
         self.row = row
         self.col = col
-        self.health = None
+        self.max_health = None
+        self.current_health = None
         self.attack_power = None
         self.healing_rate = None
 
         if unit_type == 'earth':
-            self.health = 18
+            self.max_health = 18
+            self.current_health = self.max_health
             self.attack_power = 2
             self.healing_rate = 3
         
         elif unit_type == 'fire':
-            self.health = 12
+            self.max_health = 12
+            self.current_health = self.max_health
             self.attack_power = 4
             self.healing_rate = 1
         
         elif unit_type == 'water':
-            self.health = 14
+            self.max_health = 14
+            self.current_health = self.max_health
             self.attack_power = 3
             self.healing_rate = 2
         
         elif unit_type == 'air':
-            self.health = 10
+            self.max_health = 10
+            self.current_health = self.max_health
             self.attack_power = 2
             self.healing_rate = 2
 
